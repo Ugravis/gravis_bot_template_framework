dffe23c53537cdd6a979790bcd3a8c7d4f7667bb
Ulysse Pennetier <upennetier@gmail.com>
2026-01-30 15:52:21 +0100

    feat(index): demo script from docs + dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-// Reset
+import dotenv from 'dotenv'
+dotenv.config({ quiet: true })
+
+import { Client, Events, GatewayIntentBits } from 'discord.js'
+
+const client = new Client({ 
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildPresences
+  ] 
+})
+
+client.once(Events.ClientReady, (readyClient) => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`)
+})
+
+client.login(process.env.DEV_BOT_TOKEN)
