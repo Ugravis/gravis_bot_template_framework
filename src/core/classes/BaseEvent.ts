a87a02d5836614fed4dfa700ed4849932d827205
Ulysse Pennetier <upennetier@gmail.com>
2026-01-30 15:35:41 +0100

    * --- reset done --- *

--- src/core/classes/BaseEvent.ts
+++
@@ -1,9 +0,0 @@
-import { MyClient } from "../client/MyClient"
-
-export abstract class BaseEvent<T extends any[]> {
-  public client!: MyClient
-  abstract name: string
-  abstract description: string
-
-  abstract execute(...args: T): Promise<void>
-}
